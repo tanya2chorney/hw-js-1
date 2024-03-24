7a73fac72d4982fabbe9a5186fa400de32ecee09
tanya2chorney <tanyachorney5@gmail.com>
2024-03-24 10:40:21 +0200

    added variable myNull

--- js/one.js
+++ js/one.js
@@ -8,8 +8,11 @@ const isStudent = true;
 console.log(isStudent);
 
 let myNumber = 15;
-let = 25;
+// let = 25;
 console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
 
 let inputName = prompt("enter your name");
 alert("Hi "+inputName+" !");
