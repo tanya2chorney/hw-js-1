198fbc42b2fe8e16ae1d551fc04e7bae6e4ed100
tanya2chorney <tanyachorney5@gmail.com>
2024-03-24 10:28:34 +0200

    add js

---
+++ js/one.js
@@ -0,0 +1,17 @@
+const age = 16;
+console.log(age);
+
+const myName = "Tanya";
+console.log(myName);
+
+const isStudent = true;
+console.log(isStudent);
+
+let myNumber = 15;
+let = 25;
+console.log(myNumber);
+
+let inputName = prompt("enter your name");
+alert("Hi "+inputName+" !");
+
+
