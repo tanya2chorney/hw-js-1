f4bc20d0afd2e1f3363ff8fbb1bed7b8120d7c6d
tanya2chorney <tanyachorney5@gmail.com>
2024-03-28 11:43:22 +0200

    hw-2

--- js/one.js
+++ js/one.js
@@ -1,23 +1,33 @@
-const age = 16;
-console.log(age);
+// const age = 16;
+// console.log(age);
 
-const myName = "Tanya";
-console.log(myName);
+// const myName = "Tanya";
+// console.log(myName);
 
-const isStudent = true;
-console.log(isStudent);
+// const isStudent = true;
+// console.log(isStudent);
 
-const myString = "It is risky to live, you can die";
-console.log(myString);
+// const myString = "It is risky to live, you can die";
+// console.log(myString);
 
-let myNumber = 15;
-myNumber = 25;
-console.log(myNumber);
+// let myNumber = 15;
+// myNumber = 25;
+// console.log(myNumber);
 
-const myNull = null;
-console.log(myNull);
+// const myNull = null;
+// console.log(myNull);
 
-let inputName = prompt("enter your name");
-alert("Hi "+inputName+" !");
+// let inputName = prompt("enter your name");
+// alert("Hi "+inputName+" !");
+
+let number = Number(prompt("Please enter your number"));
+console.log(number);
 
 
+let secondNumber = Number(prompt("Please enter your power"));
+// secondNumber =  Number(secondNumber);
+console.log(secondNumber);
+
+// const numberInPower = Math.pow(number , secondNumber);
+const numberInPower = number ** secondNumber;
+alert("your result: "+numberInPower);
---
+++ js/two.js
@@ -0,0 +1,48 @@
+// Завдання №1
+
+let celsiusTemperature = 10;
+const fahrenheitTemperature = 15;
+
+const fromCelsiusToFahrenheit = (celsiusTemperature * 9 / 5) + 32;
+console.log(fromCelsiusToFahrenheit);
+
+// Завдання №2
+
+const days = 30;
+const hours = (days * 24);
+console.log("Кількість годин в місяці :", hours);
+const minutes = (hours * 60);
+console.log("Кількість хвилин в місяці :", minutes);
+
+// Завдання №3
+
+let energy = 100;
+console.log(energy = (energy - 30));
+let health = 100;
+console.log(health = (health- 50));
+
+// Завдання №4
+
+const amount = 1500;
+const discount = 0.1;
+const discountedAmount = (amount *(1 - discount ));
+console.log(discountedAmount);
+
+// Завдання №5
+
+let number = 12.668;
+console.log(number = Math.floor(number));
+
+// Завдання №6
+
+let numberFloat = "15,588";
+console.log(numberFloat = parseFloat( numberFloat));
+
+// Завдання №7
+
+let int = "20";
+console.log(int = parseInt(int));
+
+// Завдання №8
+let sqrt = 25;
+console.log(sqrt = Math.sqrt(sqrt));
