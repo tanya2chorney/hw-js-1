745215391579bd09dd058a27bdd55ffb9abf416a
tanya2chorney <tanyachorney5@gmail.com>
2024-04-08 13:54:21 +0300

    hw-3

---
+++ js/hw-3.js
@@ -0,0 +1,21 @@
+// Завдання 1
+const result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+// Завдання 2
+const email = "tanyachorney214@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+// Завдання 3
+const firstWord = `My`;
+const secondWord = `name`;
+const thirdWord = `is`;
+const fullName = `${firstWord} ${secondWord} ${thirdWord} Viktor.`
+console.log(fullName);
+
+// Завдання 4 
+const userName = `Олександро!`;
+const payment = 300; 
+alert(`Дякуємо, ` + userName + ` До сплати ` + payment + ' гривень.')
